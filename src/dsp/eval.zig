4c0bba9117ea1de30fc8719b3cac09d51a889930
Joseph DALY <joseph300905@gmail.com>
2025-10-17 18:20:53 +0200

    add multiple channels to blocks

--- src/dsp/eval.zig
+++ src/dsp/eval.zig
@@ -4,24 +4,24 @@ pub fn VecF32(comptime length: u8) type {
     return @Vector(length, f32);
 }
 
-pub fn Block(comptime block_length: u32, comptime simd_length: u8) type {
+pub fn Block(comptime channels: u8, comptime block_length: u32, comptime simd_length: u8) type {
     const Vec = VecF32(simd_length);
 
     const vecs_per_block = block_length / simd_length;
 
     return struct {
-        vectors: [vecs_per_block]Vec,
+        channels: [channels][vecs_per_block]Vec,
 
         pub fn init() @This() {
-            return @This(){ .vectors = std.mem.zeroes([vecs_per_block]Vec) };
+            return @This(){ .channels = std.mem.zeroes([channels][vecs_per_block]Vec) };
         }
 
-        pub fn get(self: *const @This(), idx: u32) f32 {
-            return self.vectors[idx / simd_length][idx % simd_length];
+        pub fn get(self: *const @This(), channel: u8, idx: u32) f32 {
+            return self.channels[channel][idx / simd_length][idx % simd_length];
         }
 
-        pub fn set(self: *@This(), idx: u32, val: f32) void {
-            self.vectors[idx / simd_length][idx % simd_length] = val;
+        pub fn set(self: *@This(), channel: u8, idx: u32, val: f32) void {
+            self.channels[channel][idx / simd_length][idx % simd_length] = val;
         }
     };
 }
@@ -29,13 +29,15 @@ pub fn Block(comptime block_length: u32, comptime simd_length: u8) type {
 pub fn EvalOperators(comptime BlockType: type) type {
     return struct {
         pub fn add(stack: []BlockType, sp: *usize) void {
-            const b = stack[sp.* - 1];
-            const a = stack[sp.* - 2];
+            const b = &stack[sp.* - 1];
+            const a = &stack[sp.* - 2];
 
             var result: BlockType = undefined;
 
-            for (a.vectors, b.vectors, 0..) |a_vec, b_vec, i| {
-                result.vectors[i] = a_vec + b_vec;
+            for (a.channels, b.channels, 0..) |a_channel, b_channel, i| {
+                for (a_channel, b_channel, 0..) |a_vec, b_vec, j| {
+                    result.channels[i][j] = a_vec + b_vec;
+                }
             }
 
             stack[sp.* - 2] = result;
