e5c43bb7dc8068bdc0b86890abb67287fa08845f
Joseph DALY <joseph300905@gmail.com>
2025-10-17 20:36:14 +0200

    single engine type

--- src/dsp/eval.zig
+++ src/dsp/eval.zig
@@ -1,19 +1,14 @@
 const std = @import("std");
 
-pub fn VecF32(comptime length: u8) type {
-    return @Vector(length, f32);
-}
-
-pub fn Block(comptime channels: u8, comptime block_length: u32, comptime simd_length: u8) type {
-    const Vec = VecF32(simd_length);
+pub fn Engine(comptime channel_count: u8, comptime block_length: u32, comptime simd_length: u8, comptime stack_size: usize) type {
+    const Vec = @Vector(simd_length, f32);
 
     const vecs_per_block = block_length / simd_length;
-
-    return struct {
-        channels: [channels][vecs_per_block]Vec,
+    const Block = struct {
+        channels: [channel_count][vecs_per_block]Vec,
 
         pub fn init() @This() {
-            return @This(){ .channels = std.mem.zeroes([channels][vecs_per_block]Vec) };
+            return @This(){ .channels = std.mem.zeroes([channel_count][vecs_per_block]Vec) };
         }
 
         pub fn get(self: *const @This(), channel: u8, idx: u32) f32 {
@@ -23,24 +18,30 @@ pub fn Block(comptime channels: u8, comptime block_length: u32, comptime simd_le
         pub fn set(self: *@This(), channel: u8, idx: u32, val: f32) void {
             self.channels[channel][idx / simd_length][idx % simd_length] = val;
         }
-
-        pub fn vecType() type {
-            return Vec;
-        }
     };
-}
 
-pub fn EvalOperators(comptime BlockType: type) type {
-    const Vec = BlockType.vecType();
-    const Op = fn ([]BlockType, *usize) void;
-    const Helpers = struct {
-        fn generateBinaryOp(comptime op: fn (Vec, Vec) Vec) Op {
+    const Operation = union(enum) {
+        Push: *const Block,
+        Add,
+        Mul,
+        Min,
+        Max,
+    };
+
+    const BinaryOp = fn (Vec, Vec) Vec;
+    const Expr = fn ([]Block, *usize) void;
+
+    return struct {
+        sp: usize,
+        stack: [stack_size]Block,
+
+        fn generateBinaryExpr(op: BinaryOp) Expr {
             return struct {
-                pub fn generated(stack: []BlockType, sp: *usize) void {
+                fn res(stack: []Block, sp: *usize) void {
                     const b = &stack[sp.* - 1];
                     const a = &stack[sp.* - 2];
 
-                    var result: BlockType = undefined;
+                    var result: Block = undefined;
 
                     for (a.channels, b.channels, 0..) |a_channel, b_channel, i| {
                         for (a_channel, b_channel, 0..) |a_vec, b_vec, j| {
@@ -51,29 +52,50 @@ pub fn EvalOperators(comptime BlockType: type) type {
                     stack[sp.* - 2] = result;
                     sp.* -= 1;
                 }
-            }.generated;
+            }.res;
         }
-    };
-    return struct {
-        pub const add = Helpers.generateBinaryOp(struct {
-            fn op(a: Vec, b: Vec) Vec {
-                return a + b;
+
+        pub fn run(self: *@This(), instr_set: []const Operation) void {
+            for (instr_set) |op| {
+                switch (op) {
+                    .Push => {
+                        self.stack[self.sp] = op.Push.*;
+                        self.sp += 1;
+                    },
+                    .Add => generateBinaryExpr(struct {
+                        fn func(a: Vec, b: Vec) Vec {
+                            return a + b;
+                        }
+                    }.func)(&self.stack, &self.sp),
+                    .Mul => generateBinaryExpr(struct {
+                        fn func(a: Vec, b: Vec) Vec {
+                            return a * b;
+                        }
+                    }.func)(&self.stack, &self.sp),
+                    .Min => generateBinaryExpr(struct {
+                        fn func(a: Vec, b: Vec) Vec {
+                            return @min(a, b);
+                        }
+                    }.func)(&self.stack, &self.sp),
+                    .Max => generateBinaryExpr(struct {
+                        fn func(a: Vec, b: Vec) Vec {
+                            return @max(a, b);
+                        }
+                    }.func)(&self.stack, &self.sp),
+                }
             }
-        }.op);
-        pub const mul = Helpers.generateBinaryOp(struct {
-            fn op(a: Vec, b: Vec) Vec {
-                return a * b;
-            }
-        }.op);
-        pub const min = Helpers.generateBinaryOp(struct {
-            fn op(a: Vec, b: Vec) Vec {
-                return @min(a, b);
-            }
-        }.op);
-        pub const max = Helpers.generateBinaryOp(struct {
-            fn op(a: Vec, b: Vec) Vec {
-                return @max(a, b);
-            }
-        }.op);
+        }
+
+        pub fn getVecType() type {
+            return Vec;
+        }
+
+        pub fn getBlockType() type {
+            return Block;
+        }
+
+        pub fn getOperationType() type {
+            return Operation;
+        }
     };
 }
