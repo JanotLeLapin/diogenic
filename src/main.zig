c7c8db8fadec0b79ae5856935358bd4b8b10f333
Joseph DALY <joseph300905@gmail.com>
2025-11-21 22:58:23 +0100

    basic entry point

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,46 @@
 const std = @import("std");
 
+const root = @import("root.zig");
+
+const ast = @import("ast.zig");
+const parser = @import("parser.zig");
+const compiler = @import("compiler.zig");
+
+const engine = @import("dsp/engine.zig");
+
 pub fn main() !void {
-    std.debug.print("Hello, World\n", .{});
+    const src = "(sine (+ 220.0 (* 16.0 (sine 0.6 0.0))) 0.0)";
+
+    var t = parser.Tokenizer{ .src = src };
+
+    const gpa = std.heap.page_allocator;
+    var ast_arena = std.heap.ArenaAllocator.init(gpa);
+    defer ast_arena.deinit();
+
+    const ast_arena_alloc = ast_arena.allocator();
+
+    const node = try parser.parse(ast_arena_alloc, gpa, &t);
+
+    std.debug.print("tree:\n", .{});
+    try root.walk_ast(node, 0);
+
+    var instr_arena = std.heap.ArenaAllocator.init(gpa);
+    defer instr_arena.deinit();
+
+    const instr_arena_alloc = instr_arena.allocator();
+
+    var res = try compiler.compile_expr(node.data.Expr.children.getLast(), instr_arena_alloc, gpa);
+    defer res.deinit(instr_arena_alloc);
+
+    std.debug.print("rpn:\n", .{});
+    for (res.items) |instr| {
+        std.debug.print("{f}\n", .{instr});
+    }
+
+    var e = engine.Engine.init();
+    const block = try e.eval(res, gpa);
+
+    std.debug.print("evaluated: {d}\n", .{block.get(0, 0)});
+
+    try root.render_wav32("out.wav", res, &e, 22500, gpa, gpa);
 }
