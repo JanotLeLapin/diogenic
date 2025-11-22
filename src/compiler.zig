9696a80eb2231e56144db13bc56ff8fa01403363
Joseph DALY <joseph300905@gmail.com>
2025-11-22 12:34:42 +0100

    move node compilation logic to pre-order tree traversal

--- src/compiler.zig
+++ src/compiler.zig
@@ -9,28 +9,14 @@ pub fn compile_expr(root: *ast.Node, res_allocator: std.mem.Allocator, stack_all
     var pre_stack = try std.ArrayList(*ast.Node).initCapacity(stack_allocator, 32);
     defer pre_stack.deinit(stack_allocator);
 
-    var post_stack = try std.ArrayList(*ast.Node).initCapacity(stack_allocator, 32);
+    var post_stack = try std.ArrayList(instruction.Instruction).initCapacity(stack_allocator, 32);
     defer post_stack.deinit(stack_allocator);
 
     try pre_stack.append(stack_allocator, root);
 
+    var current_slot: usize = 0;
     while (pre_stack.items.len > 0) {
         const tmp = pre_stack.pop().?;
-        try post_stack.append(stack_allocator, tmp);
-
-        switch (tmp.data) {
-            .Expr => {
-                for (tmp.data.Expr.children.items) |child| {
-                    try pre_stack.append(stack_allocator, child);
-                }
-            },
-            else => {},
-        }
-    }
-
-    var current_slot: usize = 0;
-    while (post_stack.items.len > 0) {
-        const tmp = post_stack.pop().?;
 
         switch (tmp.data) {
             .Expr => {
@@ -42,14 +28,23 @@ pub fn compile_expr(root: *ast.Node, res_allocator: std.mem.Allocator, stack_all
                     },
                     else => {},
                 }
-                try res.append(res_allocator, instr);
+                try post_stack.append(stack_allocator, instr);
+
+                for (tmp.data.Expr.children.items) |child| {
+                    try pre_stack.append(stack_allocator, child);
+                }
             },
             .Value => {
                 const instr = instruction.Instruction{ .Value = tmp.data.Value };
-                try res.append(res_allocator, instr);
+                try post_stack.append(stack_allocator, instr);
             },
             else => {},
         }
+    }
+
+    while (post_stack.items.len > 0) {
+        const tmp = post_stack.pop().?;
+        try res.append(res_allocator, tmp);
     }
 
     return res;
