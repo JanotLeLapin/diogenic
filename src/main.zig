82f8a0a0532e55ce5e3b7ae29cbf0646c65b2278
Joseph DALY <joseph300905@gmail.com>
2025-11-22 19:28:48 +0100

    free ast when not needed anymore

--- src/main.zig
+++ src/main.zig
@@ -11,32 +11,34 @@ const engine = @import("dsp/engine.zig");
 pub fn main() !void {
     const src = "(sine (+ 220.0 (* 16.0 (sine 0.6 0.0))) 0.0)";
 
-    var t = parser.Tokenizer{ .src = src };
-
     const gpa = std.heap.page_allocator;
-    var ast_arena = std.heap.ArenaAllocator.init(gpa);
-    defer ast_arena.deinit();
-
-    const ast_arena_alloc = ast_arena.allocator();
-
-    const node = try parser.parse(ast_arena_alloc, gpa, &t);
-
-    std.debug.print("tree:\n", .{});
-    try root.walk_ast(node, 0);
 
     var instr_arena = std.heap.ArenaAllocator.init(gpa);
     defer instr_arena.deinit();
 
     const instr_arena_alloc = instr_arena.allocator();
 
-    var res = try compiler.compile_expr(node.data.Expr.children.getLast(), instr_arena_alloc, gpa);
-    defer res.deinit(instr_arena_alloc);
+    const instr = insr: {
+        var t = parser.Tokenizer{ .src = src };
+
+        var ast_arena = std.heap.ArenaAllocator.init(gpa);
+        defer ast_arena.deinit();
+
+        const ast_arena_alloc = ast_arena.allocator();
+
+        const node = try parser.parse(ast_arena_alloc, gpa, &t);
+
+        std.debug.print("tree:\n", .{});
+        try root.walk_ast(node, 0);
+
+        break :insr try compiler.compile_expr(node.data.Expr.children.getLast(), instr_arena_alloc, gpa);
+    };
 
     std.debug.print("rpn:\n", .{});
-    for (res.items) |instr| {
-        std.debug.print("{f}\n", .{instr});
+    for (instr.items) |item| {
+        std.debug.print("{f}\n", .{item});
     }
 
     var e = try engine.Engine.init(gpa);
-    try root.render_wav32("out.wav", res, &e, 22500, gpa);
+    try root.render_wav32("out.wav", instr, &e, 22500, gpa);
 }
