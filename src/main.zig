f9163efc7b679e235ae821eaab87030292e4df0c
Joseph DALY <joseph300905@gmail.com>
2025-11-23 17:08:21 +0100

    move libsndfile dependent code out of root.zig

--- src/main.zig
+++ src/main.zig
@@ -3,11 +3,44 @@ const std = @import("std");
 const root = @import("root.zig");
 
 const ast = @import("ast.zig");
+const instruction = @import("instruction.zig");
 const parser = @import("parser.zig");
 const compiler = @import("compiler.zig");
 
 const engine = @import("dsp/engine.zig");
 const block = @import("dsp/block.zig");
+
+const sndfile = @cImport({
+    @cInclude("sndfile.h");
+});
+
+pub fn renderWav32(
+    filename: []const u8,
+    instructions: []instruction.Instruction,
+    e: *engine.Engine,
+    block_count: usize,
+    buf_allocator: std.mem.Allocator,
+) !void {
+    var buf: [block.BLOCK_LENGTH * 2]f32 = undefined;
+
+    var sfinfo = sndfile.SF_INFO{
+        .frames = @intCast(block_count * block.BLOCK_LENGTH),
+        .channels = 2,
+        .samplerate = 48000,
+        .format = sndfile.SF_FORMAT_WAV | sndfile.SF_FORMAT_FLOAT,
+    };
+
+    const c_filename = try buf_allocator.dupeZ(u8, filename);
+    defer buf_allocator.free(c_filename);
+
+    const f = sndfile.sf_open(c_filename, sndfile.SFM_WRITE, &sfinfo);
+    defer _ = sndfile.sf_close(f);
+
+    for (0..block_count) |_| {
+        try root.renderBlock(instructions, e, &buf, 0);
+        _ = sndfile.sf_write_float(f, &buf, block.BLOCK_LENGTH * 2);
+    }
+}
 
 pub fn main() !void {
     const src = "(sine (+ 220.0 (* 16.0 (sine 0.6 0.0))) 0.0)";
@@ -43,7 +76,7 @@ pub fn main() !void {
     const block_count = 22500;
     var e = try engine.Engine.init(gpa);
     var timer = try std.time.Timer.start();
-    try root.renderWav32("out.wav", instr.items, &e, block_count, gpa);
+    try renderWav32("out.wav", instr.items, &e, block_count, gpa);
     const time = timer.read();
     std.log.info("Rendered {d} blocks ({d} samples), took {d}ms\n", .{ block_count, block_count * block.BLOCK_LENGTH, time / 1_000_000 });
 }
--- src/root.zig
+++ src/root.zig
@@ -6,10 +6,6 @@ const instruction = @import("instruction.zig");
 const block = @import("dsp/block.zig");
 const engine = @import("dsp/engine.zig");
 
-const sndfile = @cImport({
-    @cInclude("sndfile.h");
-});
-
 pub fn renderBlock(
     instructions: []instruction.Instruction,
     e: *engine.Engine,
@@ -20,34 +16,6 @@ pub fn renderBlock(
     for (0..block.BLOCK_LENGTH) |i| {
         out[out_offset + i * 2] = res.get(0, i);
         out[out_offset + i * 2 + 1] = res.get(1, i);
-    }
-}
-
-pub fn renderWav32(
-    filename: []const u8,
-    instructions: []instruction.Instruction,
-    e: *engine.Engine,
-    block_count: usize,
-    buf_allocator: std.mem.Allocator,
-) !void {
-    var buf: [block.BLOCK_LENGTH * 2]f32 = undefined;
-
-    var sfinfo = sndfile.SF_INFO{
-        .frames = @intCast(block_count * block.BLOCK_LENGTH),
-        .channels = 2,
-        .samplerate = 48000,
-        .format = sndfile.SF_FORMAT_WAV | sndfile.SF_FORMAT_FLOAT,
-    };
-
-    const c_filename = try buf_allocator.dupeZ(u8, filename);
-    defer buf_allocator.free(c_filename);
-
-    const f = sndfile.sf_open(c_filename, sndfile.SFM_WRITE, &sfinfo);
-    defer _ = sndfile.sf_close(f);
-
-    for (0..block_count) |_| {
-        try renderBlock(instructions, e, &buf, 0);
-        _ = sndfile.sf_write_float(f, &buf, block.BLOCK_LENGTH * 2);
     }
 }
 
