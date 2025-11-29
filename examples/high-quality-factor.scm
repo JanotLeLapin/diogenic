2ac01c7698dd968eb1b064c3b3f23fab446e2f23
Joseph DALY <joseph300905@gmail.com>
2025-11-29 16:54:36 +0100

    throw in irrelevant panning in example

--- examples/high-quality-factor.scm
+++ examples/high-quality-factor.scm
@@ -2,7 +2,8 @@
       (atan (lowpass :freq (+ 250.0
                               (* 75.0
                                  (sine :freq (* 0.1 (+ 0.3 (sine :freq 0.1)))
-                                       :phase (* (+ 0.5 (* 0.5 (sine :freq 0.35))) (white-noise)))))
+                                       :phase (+ (* (+ 0.5 (* 0.5 (sine :freq 0.35))) (white-noise))
+                                                 (* 0.3 (pan (sine :freq 0.12) (sine :freq 0.6)))))))
                      :quality (exp2 (* 4.0
                                        (+ 1.25 (sine :freq 0.3))))
                      :input (sawtooth :freq 44.0))))
