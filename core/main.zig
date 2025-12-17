a5e16060a0bf40704fd7250eb0ca9eb7bd6aa86c
Joseph DALY <joseph300905@gmail.com>
2025-12-17 14:16:17 +0100

    custom log function

--- core/main.zig
+++ core/main.zig
@@ -13,6 +13,39 @@ const parser = @import("parser.zig");
 const Node = parser.Node;
 const Tokenizer = parser.Tokenizer;
 
+pub fn logFn(
+    comptime message_level: std.log.Level,
+    comptime scope: @Type(.enum_literal),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    var buffer: [64]u8 = undefined;
+    const stderr = std.debug.lockStderrWriter(&buffer);
+    const ttyconf = std.Io.tty.Config.detect(std.fs.File.stderr());
+    defer std.debug.unlockStderrWriter();
+    ttyconf.setColor(stderr, switch (message_level) {
+        .err => .red,
+        .warn => .yellow,
+        .info => .green,
+        .debug => .magenta,
+    }) catch {};
+    ttyconf.setColor(stderr, .bold) catch {};
+    stderr.writeAll(message_level.asText()) catch return;
+    ttyconf.setColor(stderr, .reset) catch {};
+    ttyconf.setColor(stderr, .dim) catch {};
+    ttyconf.setColor(stderr, .bold) catch {};
+    if (scope != .default) {
+        stderr.print("({s})", .{@tagName(scope)}) catch return;
+    }
+    stderr.writeAll(": ") catch return;
+    ttyconf.setColor(stderr, .reset) catch {};
+    stderr.print(format ++ "\n", args) catch return;
+}
+
+pub const std_options = std.Options{
+    .logFn = logFn,
+};
+
 pub fn compile(root: *Node, alloc: std.mem.Allocator) !std.ArrayList(Instruction) {
     var pre_stack = try std.ArrayList(*Node).initCapacity(alloc, 32);
     defer pre_stack.deinit(alloc);
