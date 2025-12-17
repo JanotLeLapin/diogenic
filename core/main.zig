a6c13001cc9a2b51e17c972483ffcc4c0ba13ea3
Joseph DALY <joseph300905@gmail.com>
2025-12-17 13:41:56 +0100

    full evaluation pass implementation

--- core/main.zig
+++ core/main.zig
@@ -6,8 +6,19 @@ const compiler = @import("compiler.zig");
 const engine = @import("engine.zig");
 const EngineState = engine.EngineState;
 
+const instruction = @import("instruction.zig");
+const Instruction = instruction.Instruction;
+
 const parser = @import("parser.zig");
 const Tokenizer = parser.Tokenizer;
+
+pub fn eval(state: *EngineState, instructions: []const Instruction) !void {
+    for (instructions) |instr| {
+        switch (instr) {
+            inline else => |device| device.eval(state),
+        }
+    }
+}
 
 pub fn main() !void {
     const input = "(* 1.5 (+ 2 3))";
