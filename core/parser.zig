51d6c029f6ebce365ece831c3d226b1cb1231fa7
Joseph DALY <joseph300905@gmail.com>
2025-12-17 11:33:38 +0100

    parse tokenizer into ast

--- core/parser.zig
+++ core/parser.zig
@@ -15,6 +15,15 @@ pub const Token = union(enum) {
     whitespace,
 };
 
+pub const Node = struct {
+    data: union(enum) {
+        list: std.ArrayList(*Node),
+        id: []const u8,
+        num: f32,
+    },
+    src: []const u8,
+};
+
 pub inline fn isWhitespace(c: u8) bool {
     return switch (c) {
         '\n', '\t', ' ' => true,
@@ -75,4 +84,58 @@ pub fn tokenizerNext(t: *Tokenizer) ?Token {
 
     const lit = t.src[start..t.cursor];
     return Token{ .lit = lit };
+}
+
+pub fn parse(
+    t: *Tokenizer,
+    ast_alloc: std.mem.Allocator,
+    stack_alloc: std.mem.Allocator,
+) !*Node {
+    var stack = try std.ArrayList(*Node).initCapacity(stack_alloc, 8);
+    defer stack.deinit(stack_alloc);
+
+    const root = try ast_alloc.create(Node);
+    root.* = .{
+        .data = .{ .list = try std.ArrayList(*Node).initCapacity(ast_alloc, 8) },
+        .src = t.src,
+    };
+    try stack.append(stack_alloc, root);
+
+    while (tokenizerNext(t)) |token| {
+        switch (token) {
+            .whitespace => continue,
+            .sep => |sep| switch (sep) {
+                '(' => {
+                    const new = try ast_alloc.create(Node);
+                    const start = t.cursor - 1;
+                    new.* = .{
+                        .data = .{ .list = try std.ArrayList(*Node).initCapacity(ast_alloc, 8) },
+                        .src = t.src[start..],
+                    };
+                    try stack.getLast().data.list.append(ast_alloc, new);
+                    try stack.append(stack_alloc, new);
+                    continue;
+                },
+                ')' => {
+                    var node = stack.pop() orelse return error.EmptyStack;
+                    const start = node.src.ptr - t.src.ptr;
+                    node.src = t.src[start..t.cursor];
+                    continue;
+                },
+                else => {},
+            },
+            else => {},
+        }
+
+        const new = try ast_alloc.create(Node);
+        new.src = token.lit;
+        if (std.fmt.parseFloat(f32, token.lit)) |num| {
+            new.data = .{ .num = num };
+        } else |_| {
+            new.data = .{ .id = token.lit };
+        }
+        try stack.getLast().data.list.append(ast_alloc, new);
+    }
+
+    return root;
 }
