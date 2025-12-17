a47768b7ad82ddb5664e181502dc1ce8a68aa783
Joseph DALY <joseph300905@gmail.com>
2025-12-17 19:10:48 +0100

    simd-friendly osc implementation for static frequencies

--- core/instruction/osc.zig
+++ core/instruction/osc.zig
@@ -12,31 +12,32 @@ const parser = @import("../parser.zig");
 const Node = parser.Node;
 
 pub const Op = fn (f32) f32;
+pub const OpVec = fn (Vec) Vec;
 
-pub fn Osc(comptime label: [:0]const u8, comptime op: Op) type {
+pub fn Osc(comptime label: [:0]const u8, comptime op: Op, comptime op_vec: OpVec) type {
     return struct {
         pub const name = label;
 
-        current_phase: usize,
+        phase_index: usize,
+        static_freq: ?f32,
 
-        pub fn compile(state: *CompilerState, _: *Node) !@This() {
-            const res = @This(){
-                .current_phase = state.state_index,
+        pub fn compile(state: *CompilerState, expr: *Node) !@This() {
+            const self = @This(){
+                .phase_index = state.state_index,
+                .static_freq = switch (expr.data.list.items[1].data) {
+                    .num => |num| num,
+                    else => null,
+                },
             };
             state.state_index += 1;
-            return res;
+            return self;
         }
 
-        pub fn eval(self: *const @This(), state: *EngineState) void {
-            const pm = state.popStack();
-            const freq = state.popStack();
-            const out = state.reserveStack();
-            const phase = &state.state[self.current_phase];
-
+        pub fn evalDynamic(state: *EngineState, freq: *const Block, pm: *const Block, phase: *f32, out: *Block) void {
             var acc: f32 = 0.0;
-            for (freq.channels, pm.channels, 0..) |freq_channel, pm_channel, i| {
+            for (freq.channels, pm.channels, 0..) |freq_chan, pm_chan, i| {
                 acc = phase.*;
-                for (freq_channel, pm_channel, 0..) |freq_vec, pm_vec, j| {
+                for (freq_chan, pm_chan, 0..) |freq_vec, pm_vec, j| {
                     const inc_vec = freq_vec / @as(Vec, @splat(state.sr));
                     for (0..engine.SIMD_LENGTH) |k| {
                         const inc = inc_vec[k];
@@ -52,6 +53,47 @@ pub fn Osc(comptime label: [:0]const u8, comptime op: Op) type {
 
             phase.* = acc;
         }
+
+        pub fn evalStatic(state: *EngineState, freq: f32, pm: *const Block, phase: *f32, out: *Block) void {
+            const inc = freq / state.sr;
+
+            var ramp: Vec = undefined;
+            inline for (0..engine.SIMD_LENGTH) |i| {
+                ramp[i] = inc * @as(f32, @floatFromInt(i));
+            }
+
+            const block_inc = inc * @as(f32, @floatFromInt(engine.SIMD_LENGTH));
+
+            var acc: f32 = 0.0;
+            for (pm.channels, 0..) |pm_chan, i| {
+                acc = phase.*;
+                for (pm_chan, 0..) |pm_vec, j| {
+                    const v_phase = @as(Vec, @splat(acc)) + ramp + pm_vec;
+                    const v_phase_wrapped = v_phase - @floor(v_phase);
+
+                    const radians = @as(Vec, @splat(std.math.pi * 2)) * v_phase_wrapped;
+                    out.channels[i][j] = op_vec(radians);
+
+                    acc += block_inc;
+                    acc -= @floor(acc);
+                }
+            }
+
+            phase.* = acc;
+        }
+
+        pub fn eval(self: *const @This(), state: *EngineState) void {
+            const pm = state.popStack();
+            const freq = state.popStack();
+            const phase = &state.state[self.phase_index];
+            const out = state.reserveStack();
+
+            if (self.static_freq) |v| {
+                evalStatic(state, v, pm, phase, out);
+            } else {
+                evalDynamic(state, freq, pm, phase, out);
+            }
+        }
     };
 }
 
@@ -59,14 +101,26 @@ fn sine(p: f32) f32 {
     return std.math.sin(p);
 }
 
+fn sineVec(p: Vec) Vec {
+    return @sin(p);
+}
+
 fn sawtooth(p: f32) f32 {
     return p / std.math.pi - 1.0;
+}
+
+fn sawtoothVec(p: Vec) Vec {
+    return p / @as(Vec, @splat(std.math.pi)) - @as(Vec, @splat(1.0));
 }
 
 fn square(p: f32) f32 {
     return @floor(p / std.math.pi) * 2.0 - 1.0;
 }
 
-pub const Sine = Osc("sine", sine);
-pub const Sawtooth = Osc("sawtooth", sawtooth);
-pub const Square = Osc("square", square);
+fn squareVec(p: Vec) Vec {
+    return @floor(p / @as(Vec, @splat(std.math.pi))) * @as(Vec, @splat(2.0)) - @as(Vec, @splat(1.0));
+}
+
+pub const Sine = Osc("sine", sine, sineVec);
+pub const Sawtooth = Osc("sawtooth", sawtooth, sawtoothVec);
+pub const Square = Osc("square", square, squareVec);
