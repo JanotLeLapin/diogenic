b5d75b931809bf9d829ed50af2a33ba43c827266
Joseph DALY <joseph300905@gmail.com>
2025-12-17 10:50:42 +0100

    quick s-expression tokenizer

---
+++ core/parser.zig
@@ -0,0 +1,80 @@
+const std = @import("std");
+
+pub const Tokenizer = struct {
+    src: []const u8,
+    cursor: usize = 0,
+
+    pub inline fn getChar(self: *const Tokenizer) u8 {
+        return self.src[self.cursor];
+    }
+};
+
+pub const Token = union(enum) {
+    sep: u8,
+    id: []const u8,
+    num: f32,
+    whitespace,
+};
+
+pub inline fn isWhitespace(c: u8) bool {
+    return switch (c) {
+        '\n', '\t', ' ' => true,
+        else => false,
+    };
+}
+
+pub fn tokenizerSkipWhitespace(t: *Tokenizer) ?Token {
+    if (!isWhitespace(t.getChar())) {
+        return null;
+    }
+
+    t.cursor += 1;
+    while (true) {
+        if (t.cursor >= t.src.len) {
+            return .whitespace;
+        }
+
+        if (!isWhitespace(t.getChar())) {
+            return .whitespace;
+        }
+        t.cursor += 1;
+    }
+}
+
+pub fn tokenizerNext(t: *Tokenizer) ?Token {
+    if (t.cursor >= t.src.len) {
+        return null;
+    }
+
+    if (tokenizerSkipWhitespace(t)) |token| {
+        return token;
+    }
+
+    switch (t.getChar()) {
+        '(', ')' => {
+            const token = Token{ .sep = t.getChar() };
+            t.cursor += 1;
+            return token;
+        },
+        else => {},
+    }
+
+    const start = t.cursor;
+    t.cursor += 1;
+    while (true) {
+        if (t.cursor >= t.src.len or isWhitespace(t.getChar())) {
+            break;
+        }
+
+        switch (t.getChar()) {
+            '(', ')' => break,
+            else => {},
+        }
+
+        t.cursor += 1;
+    }
+
+    const lit = t.src[start..t.cursor];
+    const num = std.fmt.parseFloat(f32, lit) catch return Token{ .id = lit };
+    return Token{ .num = num };
+}
