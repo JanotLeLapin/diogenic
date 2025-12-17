44e57cdd28a905599c61352a514adb85c410dd3b
Joseph DALY <joseph300905@gmail.com>
2025-12-17 12:58:35 +0100

    basic parser usage in main

--- core/main.zig
+++ core/main.zig
@@ -1,25 +1,25 @@
 const std = @import("std");
+const log = std.log.scoped(.core);
+
+const engine = @import("engine.zig");
+const EngineState = engine.EngineState;
+
+const parser = @import("parser.zig");
+const Tokenizer = parser.Tokenizer;
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-}
+    const input = "(* 1.5 (+ 2 3))";
+    var tokenizer = Tokenizer{ .src = input };
 
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
 
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    var ast_alloc = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer ast_alloc.deinit();
+
+    const root = try parser.parse(&tokenizer, ast_alloc.allocator(), gpa.allocator());
+    _ = root;
+
+    var e = try EngineState.init(gpa.allocator());
+    defer e.deinit();
 }
