20c23839a3bf33988b467936e109b760867b93d8
Joseph DALY <joseph300905@gmail.com>
2025-12-19 23:15:12 +0100

    make noise work for wasm

--- core/instruction/noise.zig
+++ core/instruction/noise.zig
@@ -12,16 +12,25 @@ const Node = parser.Node;
 pub const Noise = struct {
     pub const name = "noise!";
 
-    pub fn compile(_: *CompilerState, node: *Node) !@This() {
+    state_idx: usize,
+
+    pub fn compile(state: *CompilerState, node: *Node) !@This() {
         if (node.data.list.items.len != 1) {
             return error.BadArity;
         }
-        return @This(){};
+        const self = @This(){ .state_idx = state.state_index };
+        state.state_index += 1;
+        return self;
     }
 
-    pub fn eval(_: *const @This(), state: *EngineState) void {
-        var prng = std.Random.DefaultPrng.init(@intCast(std.time.nanoTimestamp()));
+    pub fn eval(self: *const @This(), state: *EngineState) void {
+        const seed: u64 = (@as(u64, @intCast(@as(u32, @bitCast(state.state[self.state_idx]))))) << 8 | @as(u32, @bitCast(state.state[self.state_idx + 1]));
+        var prng = std.Random.DefaultPrng.init(seed);
         var rand = prng.random();
+
+        const next_seed: u64 = rand.int(u64);
+        state.state[self.state_idx] = @bitCast(@as(u32, @truncate(next_seed >> 32)));
+        state.state[self.state_idx + 1] = @bitCast(@as(u32, @truncate(next_seed)));
 
         const out = state.reserveStack();
         for (&out.channels) |*out_chan| {
