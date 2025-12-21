84c567dedb8c14c2683b2d32ae4a6fa280852cee
Joseph DALY <joseph300905@gmail.com>
2025-12-22 00:29:02 +0100

    examples: explicit lowpass arguments

--- examples/3-high-quality-factor.scm
+++ examples/3-high-quality-factor.scm
@@ -1,11 +1,9 @@
 ; this may be a dangerous patch for your
 ; ears or your audio devices
-(clip 1.0
-      (atan (b-lowpass! (+ 250.0
-                           (* 75.0
-                              (sine! (* 0.1 (+ 0.3 (sine! 0.1 0.0)))
-                                     (+ (* (+ 0.5 (* 0.5 (sine! 0.35 0.0))) (noise!)) 0.0))))
-                        (exp2 (* 4.0
+(sigmoid (b-lowpass! :freq (+ 250.0
+                              (* 75.0
+                                 (sine! (* 0.1 (+ 0.3 (sine! 0.1 0.0)))
+                                        (+ (* (+ 0.5 (* 0.5 (sine! 0.35 0.0))) (noise!)) 0.0))))
+                     :q (exp2 (* 4.0
                                  (+ 1.25 (sine! 0.3 0.0))))
-                        1.0
-                        (sawtooth! 44.0 0.0))))
+                     :in (sawtooth! 44.0 0.0))))
