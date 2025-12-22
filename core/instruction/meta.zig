df23b2c805c21890fc69a7e67d3773ea86029b5a
Joseph DALY <joseph300905@gmail.com>
2025-12-22 18:16:52 +0100

    remove unused buildArgs function

--- core/instruction/meta.zig
+++ core/instruction/meta.zig
@@ -5,15 +5,3 @@ pub const Arg = struct {
     description: ?[]const u8 = null,
     default: ?f32 = null,
 };
-
-pub fn buildArgs(comptime args: []const Arg) std.StaticStringMap(Arg) {
-    const T = std.meta.Tuple(&.{ []const u8, Arg });
-    const kv_pairs = comptime blk: {
-        var arr: [args.len]T = undefined;
-        for (args, 0..) |arg, i| {
-            arr[i] = .{ arg.name, arg };
-        }
-        break :blk arr;
-    };
-    return std.StaticStringMap(Arg).initComptime(kv_pairs);
-}
