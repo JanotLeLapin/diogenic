e340dacfb20df5c4f3ef299c2f9cd1205ebe6059
Joseph DALY <joseph300905@gmail.com>
2025-12-23 15:44:39 +0100

    examples: remap output signal range

--- examples/3-high-quality-factor.scm
+++ examples/3-high-quality-factor.scm
@@ -1,10 +1,11 @@
 ; this may be a dangerous patch for your
 ; ears or your audio devices
-(sigmoid
-  (b-lowpass! :freq (+ 250.0
-                       (* 75.0
-                          (sine! (* 0.1 (+ 0.3 (sine! 0.1)))
-                                 (+ (* (+ 0.5 (* 0.5 (sine! 0.35))) (noise!)) 0.0))))
-              :q (exp2 (* 4.0
-                          (+ 1.26 (sine! (+ 0.53 (* 0.4 (sawtooth! 0.02)))))))
-              :in (sawtooth! (+ 44.0 (* 4.0 (+ (sine! 0.076) (sawtooth! 0.05))))))))
+(- (* 2.0 (sigmoid
+            (b-lowpass! :freq (+ 250.0
+                                 (* 75.0
+                                    (sine! (* 0.1 (+ 0.3 (sine! 0.1)))
+                                           (+ (* (+ 0.5 (* 0.5 (sine! 0.35))) (noise!)) 0.0))))
+                        :q (exp2 (* 4.0
+                                    (+ 1.26 (sine! (+ 0.53 (* 0.4 (sawtooth! 0.02)))))))
+                        :in (sawtooth! (+ 44.0 (* 4.0 (+ (sine! 0.076) (sawtooth! 0.05))))))))
+   1.0)
