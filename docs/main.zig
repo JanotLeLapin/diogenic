9c0b028501b3d2d0b75ac4bff2d960e3b0d81224
Joseph DALY <joseph300905@gmail.com>
2026-01-17 14:19:02 +0100

    generate doc for undocumented args & devices

--- docs/main.zig
+++ docs/main.zig
@@ -5,29 +5,26 @@ const core = @import("diogenic-core");
 pub fn main() void {
     std.debug.print("*This version of diogenic includes `{d}` devices.*\n\n", .{core.instruction.Instructions.len});
     inline for (core.instruction.Instructions) |T| {
-        if (@hasDecl(T, "description")) {
+        if (T.name[0] != '_') {
+            const desc = if (@hasDecl(T, "description")) T.description else "*no description provided*";
             std.debug.print(
-                "### `" ++ T.name ++ "`\n\n" ++ T.description ++ ". `{d}` argument{s}.\n\n",
+                "### `" ++ T.name ++ "`\n\n" ++ desc ++ ". `{d}` argument{s}.\n\n",
                 .{
                     T.args.len,
                     if (T.args.len == 1) "" else "s",
                 },
             );
             if (T.args.len > 0) {
-                var flag = false;
                 inline for (T.args) |arg| {
-                    if (arg.description) |description| {
-                        flag = true;
-                        std.debug.print("- " ++ arg.name, .{});
-                        if (arg.default) |default| {
-                            std.debug.print(", default: `{d}`", .{default});
-                        }
-                        std.debug.print(", " ++ description ++ "\n", .{});
+                    std.debug.print("- " ++ arg.name, .{});
+                    if (arg.default) |default| {
+                        std.debug.print(", default: `{d}`", .{default});
+                    }
+                    if (arg.description) |a_desc| {
+                        std.debug.print(", " ++ a_desc ++ "\n", .{});
                     }
                 }
-                if (flag) {
-                    std.debug.print("\n", .{});
-                }
+                std.debug.print("\n", .{});
             }
         }
     }
