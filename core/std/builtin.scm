10227a6838a64ba534c1d60107c4ad56932fcc0f
Joseph DALY <joseph300905@gmail.com>
2026-01-20 21:34:30 +0100

    add comb filter builtin abstraction

--- core/std/builtin.scm
+++ core/std/builtin.scm
@@ -5,3 +5,7 @@
               :speed (exp2 (/ target-pitch 12.0))
               :position (< target-pitch 0.0)
               :in in)))
+
+(defun builtin/comb (in delay depth polarity)
+  (+ (* in (- 1 (* 0.5 depth)))
+     (* (* polarity (delay! in delay)) (* 0.5 depth))))
