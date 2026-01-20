943607dad32f32903d837e943d2394ebfb7a07c5
Joseph DALY <joseph300905@gmail.com>
2026-01-20 02:46:54 +0100

    examples: add blizzard

---
+++ examples/9-blizzard.scm
@@ -0,0 +1,18 @@
+(use std/builtin)
+
+(defun awesome-lfo (freq)
+  (* 0.5
+     (+ (square! (* (+ 2.0 (sine! 0.1))
+                    freq))
+        (triangle! freq))))
+
+(defun carrier ()
+  (sine! :freq (* 440.0
+                  (-> (triangle! :freq (awesome-lfo 2.0))
+                      (tanh)
+                      (foldback 0.9)
+                      (bi->uni)))))
+
+(builtin/pitch-shift (+ 24.0 (* 12.0 (sawtooth! 0.089)))
+                     (+ 75.0 (* 50.0 (sine! 0.12)))
+                     (carrier))
