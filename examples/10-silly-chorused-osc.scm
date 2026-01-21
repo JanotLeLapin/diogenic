64f69c62d7759dc23749a40eff1f0cd35ebd4fd7
Joseph DALY <joseph300905@gmail.com>
2026-01-21 21:19:30 +0100

    examples: add silly chorused osc

---
+++ examples/10-silly-chorused-osc.scm
@@ -0,0 +1,25 @@
+(use std/builtin)
+
+(defun my-osc (max-factor)
+  (let (factor (map (sine! 0.6) -1 1 2 max-factor)
+        note (bi->uni (triangle! 0.1)))
+    (sine! (-> note
+               (* factor)
+               (floor)
+               (/ factor)
+               (* 12.0)
+               (+ 48.0)
+               (midi->freq)))))
+
+(defun my-limiter (in gain)
+  (clip (* in gain) 1.0))
+
+(defun my-chorus (in)
+  (builtin/comb in
+                (map (sine! 0.08) -1 1 0.09 0.017)
+                0.6
+                -1))
+
+(-> (my-osc 20)
+    (my-limiter (map (triangle! 0.08) -1 1 1.2 1.6))
+    (my-chorus))
