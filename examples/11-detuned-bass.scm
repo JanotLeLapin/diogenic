d2d80c11f97928ac482e6b3ccfa6cf2b32201231
Joseph DALY <joseph300905@gmail.com>
2026-01-24 18:07:24 +0100

    examples: casually make detuned bass sound 10x more cool

--- examples/11-detuned-bass.scm
+++ examples/11-detuned-bass.scm
@@ -1,14 +1,23 @@
-(defun dephased-bass (freq detune)
+(defun dephased-sawtooth (freq detune)
   "two sawtooths, slight detune"
+  (let (offset (* 0.5 detune))
+    (+ (sawtooth! (+ freq offset))
+       (sawtooth! (- freq offset)))))
+
+(defun dephased-bass (freq detune)
+  "dephased, clipped, quantized sawtooth bass"
   (-> (let (offset (* 0.5 detune))
-        (+ (sawtooth! (+ freq offset))
-           (sawtooth! (- freq offset))))
+        (+ (dephased-sawtooth freq (* 0.5 detune))
+           (pan (dephased-sawtooth (* 3.0 freq) (* 1.5 detune))
+                (* 0.6 (sine! 0.067)))))
+      (* 1.6)
       (tanh)
       (clip 0.6)
       (b-lowpass!
         :freq (map (sine! 0.085) -1 1 120 600)
-        :q 0.6)))
+        :q 0.6)
+      (quantize 5.4)))
 
 (dephased-bass
-  55.0
+  (map (sine! (map (sine! 0.06) -1 1 0.06 0.46)) -1 1 40 60)
   (* 0.3 (bi->uni (triangle! 0.1))))
