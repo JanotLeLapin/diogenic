627de72c72acf74a2ddd0a755fb8e80b688503ec
Joseph DALY <joseph300905@gmail.com>
2026-01-28 14:48:40 +0100

    add core/compiler.zig

---
+++ core/compiler.zig
@@ -0,0 +1,6 @@
+pub const function = @import("compiler/function.zig");
+pub const module = @import("compiler/module.zig");
+pub const rpn = @import("compiler/rpn.zig");
+
+pub const exception = @import("compiler/exception.zig");
+pub const types = @import("compiler/types.zig");
